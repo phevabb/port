6d718ed5a67421dcdd1e73fba6cf3d350469a69c
phevab <phevab1@gmail.com>
2025-07-04 22:53:45 +0000

    switched to neon

--- core/settings.py
+++ core/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from urllib.parse import urlparse, parse_qsl
+from decouple import config
 import cloudinary_storage
 from pathlib import Path
 import os
@@ -79,11 +81,19 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+tmpPostgres = config('DATABASE_URL')
+
+parsed_url = urlparse(tmpPostgres)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'deterministic': False,
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': parsed_url.path[1:],  # remove leading '/'
+        'USER': parsed_url.username,
+        'PASSWORD': parsed_url.password,
+        'HOST': parsed_url.hostname,
+        'PORT': parsed_url.port or 5432,
+        'OPTIONS': dict(parse_qsl(parsed_url.query)),
     }
 }
 
